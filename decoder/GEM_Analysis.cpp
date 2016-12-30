4d46e64130430d9f7ecafc666583f21498850521
Jiansiyu <jiansiyu@gmail.com>
2016-12-29 19:27:49 -0500

    ciae release version 1

--- decoder/GEM_Analysis.cpp
+++ decoder/GEM_Analysis.cpp
@@ -54,12 +54,12 @@ void Usage()
   }
 
 struct globalViabArgs_t {
-	const char *Run_mode;
-	const char *input_rawfilenames;
-	const char *input_pedestal_name;
-	const char *output_filename;
-	const char *pedestal_filename;
-	const char *batch_mode_filename;
+	const char *Run_mode=NULL;
+	const char *input_rawfilenames=NULL;
+	const char *input_pedestal_name=NULL;
+	const char *output_filename=NULL;
+	const char *pedestal_filename=NULL;
+	const char *batch_mode_filename=NULL;
 };
 
 static const struct option long_options[]={
@@ -71,7 +71,9 @@ static const struct option long_options[]={
         {"version", no_argument, 0, 'v'},
         {0, 0, 0, 0}
 };
-
+void getsysinfor(){   // prepare for multi-thread usage, will add concurrent calculation soon
+	cout<<"number of cores:"<<sysconf(_SC_NPROCESSORS_ONLN)<<endl;
+}
 int main(int argc, char **argv)
 {
 		//
@@ -82,6 +84,7 @@ int main(int argc, char **argv)
 			printf("./GEM_Analysis -[option] [file or command]\n");
 			exit(0);
 		}
+
 		globalViabArgs_t globalViabArgs_temp;
 		ui_input=getopt_long(argc, argv, "vm:o:i:p:h", long_options, &option_index);
 		while(ui_input!=-1) {
@@ -116,23 +119,73 @@ int main(int argc, char **argv)
 			ui_input=getopt_long(argc,argv,"vm:o:i:p:h", long_options, &option_index);
 		};
 
+
+
 		if(globalViabArgs_temp.Run_mode==NULL){
 			printf("[%s(line%d)] Can not resolve \"RUN MODE\"\n",__FUNCTION__,__LINE__);
 			exit(-1);
 		}else{
+
 			if(globalViabArgs_temp.input_rawfilenames==NULL){
 				printf("[%s(line%d)] no input file detected\n",__FUNCTION__,__LINE__);
+				exit(-1);
 			}
 			string inputfilename_str(globalViabArgs_temp.input_rawfilenames);
 			string runmode_str(globalViabArgs_temp.Run_mode);
 			transform(runmode_str.begin(),runmode_str.end(),runmode_str.begin(),::tolower);
+
+
 			if(runmode_str=="r"){
-				cout<<"runmode = "<<runmode_str.c_str()<<endl;
+				TTree test_tree;
+				GEMRawFileDecoder *GEMRawFileDecoder_run=new GEMRawFileDecoder(inputfilename_str.c_str(),&test_tree);
+				GEMRawFileDecoder_run->GEMRawFileDecoder_RawDisplay(-1);
+				delete GEMRawFileDecoder_run;
+				//cout<<"runmode = "<<runmode_str.c_str()<<endl;
+
 			}else if(runmode_str=="p") {
+				if(globalViabArgs_temp.output_filename==NULL){
+					printf("[%s(line%d)] no output filename detected\n",__FUNCTION__,__LINE__);
+					exit(-1);
+				}else {
+					string output_filename(globalViabArgs_temp.output_filename);
+					if(output_filename.substr(output_filename.find_last_of(".")+1)!="root") {
+						printf("[%s(line%d)] output file should be a .root file\n",__FUNCTION__,__LINE__);
+						exit(-1);
+					}else{
+						TTree test_tree;
+						GEMRawFileDecoder *GEMRawFileDecoder_run=new GEMRawFileDecoder(inputfilename_str.c_str(),&test_tree);
+						GEMRawFileDecoder_run->GEMRawFileDecoder_PedestalDecoder(output_filename.c_str(),1,-1);
+						delete GEMRawFileDecoder_run;
+					}
+				}
 
 			}else if(runmode_str=="h") {
+				if(globalViabArgs_temp.input_pedestal_name==NULL){
+					printf("[%s(line%d)] no input pedestal detected\n",__FUNCTION__,__LINE__);
+					exit(-1);
+				}else{
+					if(globalViabArgs_temp.output_filename==NULL){
+						printf("[%s(line%d)] no output filename detected\n",__FUNCTION__,__LINE__);
+						exit(-1);
+					}else{
+						string pedestal_file(globalViabArgs_temp.input_pedestal_name);
+						string output_file(globalViabArgs_temp.output_filename);
 
+						TTree test_tree;
+						GEMRawFileDecoder *GEMRawFileDecoder_run=new GEMRawFileDecoder(inputfilename_str.c_str(),&test_tree);
+						GEMRawFileDecoder_run->GEMRawFileDecoder_HistoDecoder(pedestal_file.c_str(),output_file.c_str(),-1);
+					}
+				}
 			}else if(runmode_str=="z") {
+				if(globalViabArgs_temp.input_pedestal_name==NULL){
+					printf("[%s(line%d)] no input pedestal detected\n",__FUNCTION__,__LINE__);
+					exit(-1);
+				}else{
+					string pedestal_file(globalViabArgs_temp.input_pedestal_name);
+					TTree test_tree;
+					GEMRawFileDecoder *GEMRawFileDecoder_run=new GEMRawFileDecoder(inputfilename_str.c_str(),&test_tree);
+					GEMRawFileDecoder_run->GEMRawFileDecoder_ZeroSubtractionDisplay(pedestal_file.c_str(),-1);
+				}
 
 			}else if(runmode_str=="a") {
 				if(inputfilename_str.substr(inputfilename_str.find_last_of(".")+1)=="root"){
@@ -150,10 +203,10 @@ int main(int argc, char **argv)
 					GEMTracking *pGEMTrack = new GEMTracking(fChain);
 					pGEMTrack->Run(-1,globalViabArgs_temp.output_filename);
 					delete pGEMTrack;
-
 				}else{
 					printf("[%s(line%d)] .root file is needed is \"A\" mode\n",__FUNCTION__,__LINE__);
 				}
+
 			}else{
 				printf("[%s(line%d)] Can not resolve \"RUN MODE\"\n",__FUNCTION__,__LINE__);
 				exit(0);
@@ -168,6 +221,6 @@ int main(int argc, char **argv)
 			GEMRawFileDecoder_run->GEMRawFileDecoder_HistoDecoder(Form("Pedestal_%d.root",i),Form("/home/newdriver/Research/GEM_Analsys_CIAE/Result/ciae/decoder/test_result_%d.root",i),-1);
 			delete GEMRawFileDecoder_run;
 		}*/
-
+		exit(1);
 		return 0;
 }
